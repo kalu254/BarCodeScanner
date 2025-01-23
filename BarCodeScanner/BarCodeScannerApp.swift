386d6a5e650a7b4bb3dfce96c92980586d9244bd
Kalu <irungu.luka@eclectics.io>
2025-01-23 18:20:44 +0300

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Luka Macharia on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCodeScannerTests/BarCodeScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerTests.swift
+//  BarCodeScannerTests
+//
+//  Created by Luka Macharia on 23/01/2025.
+//
+
+import Testing
+@testable import BarCodeScanner
+
+struct BarCodeScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
